09693df2a095620a1a6c2c97e709fff452f527b7
lukasheini98 <116685345+lukasheini98@users.noreply.github.com>
2022-10-25 23:21:26 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var portraitImage = document.getElementById("portrait");
+var resumeDiv = document.getElementById("resumeSection");
+/*portraitImage.addEventListener("mouseover", (event) => {
+    var p = resumeDiv.querySelector("p");
+    var pElement = document.createElement("p");
+    pElement.innerHTML = "Hier ist was";
+    resumeDiv.append(pElement);
+});*/
